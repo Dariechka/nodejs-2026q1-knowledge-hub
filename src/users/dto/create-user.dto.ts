30e13f7b09e9883e372510d29e4a3c335a73221e
darya <darya@darya>
2026-04-01 12:39:21 +0200

    fix: fix post user endpoint

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,21 @@
-export interface CreateUserDto {
+import { IsString, IsNotEmpty, IsOptional, IsIn } from 'class-validator';
+
+export interface CreateUser {
   login: string;
   password: string;
   role?: 'admin' | 'editor' | 'viewer'; // defaults to 'viewer'
+}
+
+export class CreateUserDto implements CreateUser {
+  @IsString()
+  @IsNotEmpty()
+  login: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsOptional()
+  @IsIn(['admin', 'editor', 'viewer'])
+  role?: 'admin' | 'editor' | 'viewer';
 }
