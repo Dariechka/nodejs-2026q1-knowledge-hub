3b45f327c93f6731d23cf23b432796cccd05dce6
darya <darya@darya>
2026-04-29 23:56:21 +0200

    feat: add translate article endpoint

--- src/ai/ai.controller.ts
+++ src/ai/ai.controller.ts
@@ -1,9 +1,19 @@
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Body, Controller, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
 import { ArticleService } from '../article/article.service';
 import { GeminiService } from './gemini.service';
 import { SummarizeArticleDto } from './dto/summarize-article-dto';
 import { SummarizeArticleResponse } from './dto/summarize-article-response-dto';
+import { TranslateArticleDto } from './dto/translate-article-dto';
+import { TranslateArticleResponse } from './dto/translate-article-response-dto';
 
 @ApiTags('ai/articles')
 @Controller('ai/articles')
@@ -14,6 +24,7 @@ export class AiController {
   ) {}
 
   @Post(':articleId/summarize')
+  @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Summarize article' })
   @ApiBody({ type: SummarizeArticleDto })
   @ApiResponse({ status: 200, description: 'Summary is created' })
@@ -41,5 +52,39 @@ export class AiController {
       originalLength: article.content.length,
       summaryLength: summary.length,
     } satisfies SummarizeArticleResponse;
+  }
+
+  @Post(':articleId/translate')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Translate article content' })
+  @ApiBody({ type: TranslateArticleDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Article successfully translated',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Article not found',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid input or missing targetLanguage',
+  })
+  async translate(
+    @Param('articleId', new ParseUUIDPipe({ version: '4' })) articleId: string,
+    @Body() translateDto: TranslateArticleDto,
+  ): Promise<TranslateArticleResponse> {
+    const article = await this.articleService.findOne(articleId);
+    const result = await this.geminiService.translateArticle(
+      article.content,
+      translateDto.targetLanguage,
+      translateDto.sourceLanguage,
+    );
+
+    return {
+      articleId: article.id,
+      translatedText: result.translatedText,
+      detectedLanguage: result.detectedLanguage,
+    } satisfies TranslateArticleResponse;
   }
 }
--- src/ai/gemini.service.ts
+++ src/ai/gemini.service.ts
@@ -1,31 +1,62 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { createSummarizeArticlePrompt } from './summarization.prompt';
+import { createSummarizeArticlePrompt } from './prompt/summarization.prompt';
 import { firstValueFrom } from 'rxjs';
 import { MaxLength } from './dto/summarize-article-dto';
+import { translateArticlePrompt } from './prompt/translatearticle.prompt';
 
 @Injectable()
 export class GeminiService {
   private logger = new Logger(GeminiService.name);
+  private readonly apiKey: string = process.env.GEMINI_API_KEY;
+  private readonly baseUrl: string = process.env.GEMINI_API_BASE_URL;
+  private readonly model: string = process.env.GEMINI_MODEL;
+  private readonly url: string = `${this.baseUrl}/${this.model}:generateContent?key=${this.apiKey}`;
 
   constructor(private readonly httpService: HttpService) {}
 
   async fetchSummary(content: string, maxLength: MaxLength) {
-    const apiKey = process.env.GEMINI_API_KEY;
-    const baseUrl = process.env.GEMINI_API_BASE_URL;
-    const model = process.env.GEMINI_MODEL;
-
-    const url = `${baseUrl}/${model}:generateContent?key=${apiKey}`;
     const prompt = createSummarizeArticlePrompt(content, maxLength);
 
     try {
       const { data } = await firstValueFrom(
-        this.httpService.post(url, {
+        this.httpService.post(this.url, {
           contents: [{ parts: [{ text: prompt }] }],
         }),
       );
 
       return data.candidates[0].content.parts[0].text;
+    } catch (error) {
+      this.logger.error(
+        'Gemini Error: ' + (error.response?.data || error.message),
+        error,
+      );
+    }
+  }
+
+  async translateArticle(
+    content: string,
+    targetLanguage: string,
+    sourceLanguage?: string,
+  ) {
+    const prompt = translateArticlePrompt(
+      content,
+      targetLanguage,
+      sourceLanguage,
+    );
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post(this.url, {
+          contents: [{ parts: [{ text: prompt }] }],
+          generationConfig: {
+            responseMimeType: 'application/json',
+          },
+        }),
+      );
+
+      const rawContent = response.data.candidates[0].content.parts[0].text;
+      const cleanJson = rawContent.replace(/```json|```/g, '').trim();
+      return JSON.parse(cleanJson);
     } catch (error) {
       this.logger.error(
         'Gemini Error: ' + (error.response?.data || error.message),
